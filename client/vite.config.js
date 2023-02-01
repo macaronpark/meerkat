b0c4b2dfae73ffb67390d7afc8d99c15819250bc
Suzy Park <pxxxsxzy.0@gmail.com>
2023-02-01 23:05:06 +0900

    Configure path alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+        find: "@axios",
+        replacement: path.resolve(__dirname, "./src/axios"),
+      },
+      {
+        find: "@api",
+        replacement: path.resolve(__dirname, "./src/api"),
+      },
+    ],
+  },
+});
